0a1e84dbc1a7d5e1a252dad056d23ba63079f2ee
ijassper <jasspercoach@gmail.com>
2026-08-12 16:46:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ with tab1:
                     "title": title,
                     "start": start_date,
                     "end": start_date
-                    "rich_text": {"content": analysis_content }  # 분석내용
+                    "extendedProps": {"content": analysis_content }  # 분석내용
                 })
         except Exception as e:
             continue # 데이터가 일부 없어도 에러나지 않게 건너뜀
@@ -101,7 +101,7 @@ with tab1:
     
     if state.get("eventClick"):
         event_info = state["eventClick"]
-        analysis_text = event_info["event"]["rich_text"]["content"]
+        analysis_text = event_info["event"]["extendedProps"]["content"]
         show_analysis(analysis_text)        
 
 # [탭 2] 카메라 촬영 기능
